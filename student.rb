fc0d516fed9eab4781908a8a5adfc4609d763cba
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-04 14:07:27 +0100

    updated ruby

--- student.rb
+++ student.rb
@@ -1,13 +1,12 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(name = "unknown", age = 18, classroom)
+  def initialize(classroom, name = 'unknown', age = 18)
     super(name, age)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯\(ツ)/¯'
   end
-
 end
--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,11 @@
 require_relative 'person'
 
 class Teacher
-  def initialize(name = "Unknown", age = 18, specialization)
+  def initialize(specialization, name = 'unknown', age = 18)
     super(name, age)
     @specialization = specialization
   end
-  
+
   def can_use_services?
     true # Teachers can always use services
   end
