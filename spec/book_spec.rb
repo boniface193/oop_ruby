d58cb041d999cbc24d462c0068b8e11bda7eac3d
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-21 12:18:36 +0100

    updated changes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../book'
+require_relative '../app'
 
 describe Book do
   let(:book) { Book.new('Test Book', 'Test Author') }
@@ -14,4 +15,21 @@ describe Book do
       expect(book.rentals).to be_empty
     end
   end
+
+   describe Book do
+      let(:book) { Book.new('Book 1', 'Author 1') }
+      let(:person) { Student.new(1, 'Student 1', parent_permission: true) }
+
+      describe '#add_rentals' do
+         it 'should add a rental to the book' do
+            date = '2022-09-15'
+            rental = book.add_rentals(date, person)
+            expect(rental).to be_an_instance_of(Rental)
+            expect(rental.date).to eq(date)
+            expect(rental.book).to eq(book)
+            expect(rental.person).to eq(person)
+            expect(book.rentals).to include(rental)
+         end
+      end
+   end
 end
